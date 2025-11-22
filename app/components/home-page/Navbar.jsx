0a67a927fb61a52ca07b89fa273b415ce6cd0d0a
ankitNegiDev <ankitnegi9104@gmail.com>
2025-11-22 18:45:16 +0530

    added all updated component of homepage

---
+++ app/components/home-page/Navbar.jsx
@@ -0,0 +1,391 @@
+
+
+"use client";
+
+import Image from "next/image";
+// step 1 import useRouter
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect, useRef } from "react";
+import { FaArrowRight, FaHome } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";   // dropdown arrow icon
+
+
+
+export default function Navbar() {
+
+    // creating a variable using useRotuer
+    const router=useRouter();
+
+    // Mega menu open/close
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Scroll behavior states
+    const [lastScroll, setLastScroll] = useState(0);
+    const [hideNav, setHideNav] = useState(false);
+
+    // Reference for detecting click outside
+    const menuRef = useRef(null);
+
+    // Toggle Mega Menu
+    function toggleMenu() {
+        setIsOpen(!isOpen);
+    }
+
+    // Close menu manually
+    function closeMenu() {
+        setIsOpen(false);
+    }
+
+    // Close mega menu if click outside
+    useEffect(function () {
+        function handleClickOutside(event) {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return function () {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    // Hide navbar when scrolling down
+    // Show when scrolling up
+    useEffect(function () {
+        function handleScroll() {
+            const currentScroll = window.pageYOffset;
+
+            if (currentScroll > lastScroll && currentScroll > 70) {
+                // scrolling down → hide navbar
+                setHideNav(true);
+            } else {
+                // scrolling up → show navbar
+                setHideNav(false);
+            }
+
+            setLastScroll(currentScroll);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return function () {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScroll]);
+
+    // step 2 - navigating to product page -- 
+    function goToCategory(category){
+        router.push(`/products?category=${category}`); // category here will be provided by onClick event
+
+        // now to close the menue
+        setIsOpen(false);
+    }
+
+    return (
+        <div>
+
+            {/* NAVBAR */}
+            {/* <nav
+                className={
+                    "fixed top-0 left-0 w-full z-50 bg-[#4D4D4D] text-white px-0 py-3 transition-transform duration-300  " +
+                    (hideNav ? "-translate-y-full" : "translate-y-0")
+                }
+            > */}
+            {/* <nav
+                className={
+                    "fixed top-0 left-0 w-full z-50 bg-[#111111] border-b border-[#8f7c45]/40 text-white px-0 py-3 transition-transform duration-300 " +
+                    (hideNav ? "-translate-y-full" : "translate-y-0")
+                }
+            > */}
+
+            {/* <nav
+                className={
+                    "fixed top-0 left-0 w-full z-50 bg-[#1a1a1a] text-white border-b-2 border-transparent [border-image:linear-gradient(to_right,#715723,#8f7c45)_1] px-0 py-3 transition-transform duration-300 " +
+                    (hideNav ? "-translate-y-full" : "translate-y-0")
+                }
+            >  */}
+
+            <nav
+                className={
+                    "fixed top-0 left-0 w-full z-50 text-white px-0 pt-1.5 pb-1 transition-transform duration-300 " +
+                    (hideNav ? "-translate-y-full" : "translate-y-0") +
+                    " bg-[rgba(10,10,10,0.7)] backdrop-blur-md border-b-2 border-[rgba(143,124,69,0.5)]"
+                }
+            >
+                <div className="flex items-center justify-between max-w-8xl">
+
+                    {/* Logo */}
+                    <div className="text-xl font-bold cursor-pointer">
+                        <Image
+                            src="/logo.png"
+                            alt="Website Logo"
+                            width={120}
+                            height={50}
+                            priority
+                        />
+                    </div>
+
+                    {/* Middle Menu */}
+                    <div className="flex items-center gap-6 text-xl mr-10">
+
+                        {/* Home */}
+                        <div className="flex items-center gap-1 cursor-pointer hover:text-amber-300">
+                            <FaHome size={26} />
+
+                        </div>
+                        <div className="cursor-pointer hover:text-amber-300">About Us</div>
+
+                        {/* Products with dropdown arrow */}
+                        <div className="cursor-pointer flex items-center gap-2 select-none hover:text-amber-300" onClick={toggleMenu}>
+                            Products
+
+                            {/* Dropdown arrow rotates when open */}
+                            <FaChevronDown
+                                className={
+                                    "transition-transform duration-300 " +
+                                    (isOpen ? "rotate-180" : "rotate-0")
+                                }
+                                size={13}
+                            />
+                        </div>
+
+                        {/* Other menu items */}
+                        <div className="cursor-pointer hover:text-amber-300">Signature Series</div>
+                        <div className="cursor-pointer hover:text-amber-300">Blogs</div>
+                        <div className="cursor-pointer hover:text-amber-300">Contact Us</div>
+
+
+                    </div>
+
+                </div>
+            </nav>
+
+
+            {/*FULL WIDTH MEGA MENU (Brother-style)*/}
+            {isOpen && (
+                <div
+                    className="
+                        absolute left-0 top-[100px] w-full 
+                        bg-[rgba(10,10,10,0.65)] 
+                        backdrop-blur-md
+                        text-white shadow-[0_8px_30px_rgba(0,0,0,0.7)]
+                        border-t border-[#8f7c45]/40
+                        z-40 animate-slideDown"
+                    >
+                    <div ref={menuRef} className="max-w-6xl mx-auto p-8 relative">
+
+                        {/* Close button */}
+                        <button
+                            onClick={closeMenu}
+                            className="absolute top-7 right-[-40] text-2xl hover:cursor-pointer hover:text-[#8f7c45] transition"
+                        >
+                            ✕
+                        </button>
+
+                        {/* GRID */}
+                        <div className="grid grid-cols-4 gap-x-10 gap-y-12">
+
+                            {/* 1. Boundary Wall Doors */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Boundary Wall Doors
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/boundry-wall-gates.jpg"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+                                <ul className="space-y-1 text-base">
+
+                                    <li 
+                                        onClick={function callback(){goToCategory('Royal entrance');}}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Royal entrance
+                                    </li>
+                                    <li 
+                                        onClick={function callback(){goToCategory('Modern doors');}}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Modern doors
+                                    </li>
+                                    <li
+                                        onClick={function callback() { goToCategory('Modern doors'); }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Ornamental iron
+                                    </li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Modern doors'); }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Laser cut</li>
+                                </ul>
+                            </div>
+
+                            {/* 2. Brass Railings */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Brass Railings
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/wrought-iron-doors.png"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+                                <ul className="space-y-1 text-base">
+                                    <li 
+                                        onClick={function callback(){goToCategory('Single Door');}}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Single Door</li>
+                                    <li 
+                                        onClick={function callback(){goToCategory('Double Door')}}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Double Door</li>
+                                </ul>
+                            </div>
+
+                            {/* 3. Staircase Railings */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Staircase Railings
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/stair-case-railings.png"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+                                <ul className="space-y-1 text-base">
+                                    <li 
+                                        onClick={function callback() { goToCategory('Single Door') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Single Door</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Double Door') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Double Door</li>
+                                </ul>
+                            </div>
+
+                            {/* 4. Flat Entrance Doors */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Flat Entrance Doors
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/flat-entrance.png"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+                                <ul className="space-y-1 text-base">
+                                    <li 
+                                        onClick={function callback() { goToCategory('Modern') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Modern</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Wrough iron') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Wrought iron</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Ornamental') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Ornamental</li>
+                                </ul>
+                            </div>
+
+                            {/* 5. Entry Doors */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Entry Doors
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/royal-entry-doors.jpg"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+
+                                <ul className="space-y-1 text-base">
+                                    <li 
+                                        onClick={function callback() { goToCategory('Modern') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Modern</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Wrought iron') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Wrought iron</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Ornamental') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Ornamental</li>
+                                </ul>
+                            </div>
+
+                            {/* 6. Balcony Railings */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Balcony Railings
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/Balcony-railings.png"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+
+                                <ul className="space-y-1 text-base">
+                                    <li 
+                                        onClick={function callback() { goToCategory('Flat design') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Flat design</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Textured design') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Textured design</li>
+                                </ul>
+                            </div>
+
+                            {/* 7. Planters */}
+                            <div>
+                                <h3 className="font-bold mb-3 text-[#8f7c45] uppercase tracking-wide">
+                                    Planters
+                                </h3>
+                                <div className="w-[140px] h-[140px] overflow-hidden rounded-lg mb-3 shadow-lg">
+                                    <Image
+                                        src="/desiginer-entry-gates1.jpg"
+                                        alt=""
+                                        width={130}
+                                        height={110}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
+
+                                <ul className="space-y-1 text-base">
+                                    <li 
+                                        onClick={function callback() { goToCategory('Gold coated') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Gold coated</li>
+                                    <li 
+                                        onClick={function callback() { goToCategory('Laser designed') }}
+                                        className="cursor-pointer hover:text-[#8f7c45]">Laser designed</li>
+                                </ul>
+                            </div>
+
+                            {/* 8. Others (Aligned in Grid) */}
+                            <div className="flex items-center justify-start mt-10">
+                                <h3 className="text-lg md:text-xl font-bold uppercase text-[#8f7c45] tracking-wider flex items-center gap-2 cursor-pointer hover:text-white transition">
+                                    Others <FaArrowRight size={18} />
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </div>
+    );
+}
+
---
+++ app/components/home-page/ProductListing.jsx
@@ -0,0 +1,112 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+function ProductListing() {
+
+
+    // product list array - it contains all the details about product
+    let categories = [
+        { name: "Main Gates", image:  "/desiginer-entry-gates1.jpg" },
+        { name: "Designer Entry Gates", image: "/flat-entrance.png" },
+        { name: "Modern Steel Doors", image: "/modern-steel-doors.png" },
+        { name: "Premium Balcony Railings", image: "/Balcony-railings.png" },
+        { name: "Security Doors", image: "/boundry-wall-gates.jpg" },
+        { name: "Laser Cut Panels", image: "/royal-entry-doors.jpg" },
+        { name: "Boundary Wall Designs", image: "/boundry-wall-gates.jpg" }
+    ];
+
+    // using this component we are rendering each product
+    function renderItems() {
+        return categories.map(function (item, index) {
+            return (
+                <div
+                    key={index}
+                    className="flex flex-col items-center gap-4"
+                >
+
+                    {/* IMAGE CARD */}
+                    <div className="
+                        w-48 h-48 md:w-56 md:h-56 
+                        rounded-2xl overflow-hidden 
+                        border border-[#8f7c45]/70 
+                        shadow-[0_0_25px_rgba(143,124,69,0.15)]
+                        hover:shadow-[0_0_35px_rgba(143,124,69,0.25)]
+                        hover:scale-105
+                        transition-all duration-300
+                        bg-black
+                        hover:cursor-pointer
+                    ">
+                        <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={300}
+                            height={300}
+                            loading="lazy"
+                            className="w-full h-full object-cover mix-blend-lighten"
+                            fetchPriority="low"
+                        />
+                    </div>
+
+                    {/* TITLE */}
+                    <p className="text-base md:text-lg font-semibold tracking-wide text-[#8f7c45] uppercase">
+                        {item.name}
+                    </p>
+                </div>
+            );
+        });
+    }
+
+    return (
+        //! imp - fix the color issue-- later
+        // <section className="w-full py-20 px-6 bg-slate-500 text-white">
+        // <section className="w-full py-20 px-6 bg-[#0d0d0d] text-white">
+        // <section className="w-full py-20 px-6 bg-[#0f0e0a] text-white">
+        <section className="w-full py-20 px-6 bg-[#0d0d0d] text-white">
+            {/* TITLE */}
+            <div className="text-center mb-14">
+                <h2 className="text-3xl md:text-4xl font-bold text-[#8f7c45] tracking-wide">
+                    Our Premium Categories
+                </h2>
+                <p className="text-gray-300 mt-2">
+                    Explore Luxury Designs Tailored for Modern Homes
+                </p>
+            </div>
+
+            {/* GRID */}
+            <div className="
+                grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
+                gap-10 md:gap-14 justify-items-center
+            ">
+
+                {renderItems()}
+
+                {/* VIEW ALL BOX */}
+                <div className="
+                    flex flex-col items-center justify-center 
+                    w-48 h-48 md:w-56 md:h-56 
+                    rounded-2xl border border-[#8f7c45]
+                    bg-linear-to-br from-black/60 to-black/20
+                    backdrop-blur-xl shadow-lg hover:cursor-pointer
+                ">
+                    <p className="text-3xl font-bold text-[#8f7c45]">10+</p>
+                    <p className="text-gray-300 text-sm">Categories</p>
+                    <p className="text-gray-300 text-sm">Available</p>
+
+                    <button className="
+                        mt-3 px-4 py-1 border border-[#8f7c45] rounded-md 
+                        text-[#8f7c45] text-xs hover:bg-[#8f7c45] hover:text-black
+                        transition-all duration-300 hover:cursor-pointer
+                    ">
+                        VIEW ALL
+                    </button>
+                </div>
+
+            </div>
+
+        </section>
+    );
+}
+
+export default ProductListing;
