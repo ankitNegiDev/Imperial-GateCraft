c0d63bb808a531b60d4dde77b96ef1ffffe10f01
ankitNegiDev <ankitnegi9104@gmail.com>
2025-11-27 17:42:43 +0530

    added the about us route

---
+++ app/about-us/page.jsx
@@ -0,0 +1,10 @@
+import AboutUs from "../components/about-us/AboutUs";
+
+
+export default function Contact() {
+    return (
+        <main className="pt-20">
+            <AboutUs />
+        </main>
+    );
+}
