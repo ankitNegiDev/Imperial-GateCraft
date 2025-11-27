fba2d133ee0bcb5eec74db714bcd9fb29f0990e5
ankitNegiDev <ankitnegi9104@gmail.com>
2025-11-27 17:39:20 +0530

    updated the footer-logo

--- app/components/home-page/Footer.jsx
+++ app/components/home-page/Footer.jsx
@@ -1,11 +1,22 @@
 "use client";
 
-import React from "react";
-import Image from "next/image";
+import React, { useRef } from "react";
+// import Image from "next/image";
 
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaPhone, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 function Footer() {
+
+    let videoRef = useRef(null);
+
+    // creating a function that will handle 5s delay
+    function handleVideoDelay() {
+        setTimeout(function callback() {
+            if (videoRef.current) {
+                videoRef.current.play();
+            }
+        }, 5000);
+    }
     return (
         <footer className="w-full pt-20 pb-10" style={{ backgroundColor: "#0a0a0a" }}>
 
@@ -18,26 +29,42 @@ function Footer() {
             ></div>
 
             <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-4 gap-12 text-white">
-
                 {/* LOGO + ABOUT */}
-                <div>
-                    <Image
-                        src="/logo.png"
-                        width={150}
-                        height={80}
-                        alt="Logo"
-                        className="mb-5"
-                    />
+                <div className="flex flex-col items-start w-full">
+                    <div className="w-full md:h-[280px] rounded-xl overflow-hidden">
+                        <video
+                            ref={videoRef}
+                            src="/temp-video.mp4"
+                            autoPlay
+                            muted
+                            playsInline
+                            onEnded={handleVideoDelay}
+                            className="
+                                block
+                                w-full
+                                h-[39.9vh]
+                                object-cover
+                                rounded-xl
+                                overflow-hidden
+                                scale-[1.9]     
+                                origin-center 
+                                transition-all
+                                duration-500
+                            "
+                        />
+                    </div>
 
-                    <p className="text-gray-400 leading-relaxed">
+                    <p className="text-gray-400 leading-relaxed mt-4 text-base">
                         We deliver premium craftsmanship, innovative solutions, and luxury
                         digital experiences for global brands.
                     </p>
                 </div>
 
+
+
                 {/* QUICK LINKS */}
                 <div>
-                    <h3 className="text-xl font-semibold mb-4" style={{ color: "#8f7c45" }}>
+                    <h3 className="text-xl font-semibold mb-4 text-[#fac21e] ">
                         Quick Links
                     </h3>
 
@@ -52,7 +79,7 @@ function Footer() {
 
                 {/* SERVICES */}
                 <div>
-                    <h3 className="text-xl font-semibold mb-4" style={{ color: "#8f7c45" }}>
+                    <h3 className="text-xl font-semibold mb-4 text-[#fac21e] ">
                         Services
                     </h3>
 
@@ -67,7 +94,7 @@ function Footer() {
 
                 {/* CONTACT */}
                 <div>
-                    <h3 className="text-xl font-semibold mb-4" style={{ color: "#8f7c45" }}>
+                    <h3 className="text-xl font-semibold mb-4 text-[#fac21e]">
                         Contact
                     </h3>
 
@@ -84,28 +111,28 @@ function Footer() {
                     <div className="flex gap-4 mt-5">
                         <div
                             className="w-10 h-10 flex items-center justify-center rounded-full 
-                                border border-[#8f7c45] text-[#8f7c45]
+                                border border-[#fac21e]  text-[#fac21e] 
                                 hover:bg-[#8f7c45] hover:text-black cursor-pointer transition
                                 hover:shadow-[0_0_12px_#8f7c45]"
-                            >
+                        >
                             <FaFacebookF size={18} />
                         </div>
 
                         <div
                             className="w-10 h-10 flex items-center justify-center rounded-full 
-                                border border-[#8f7c45] text-[#8f7c45]
+                                border border-[#fac21e]  text-[#fac21e] 
                                 hover:bg-[#8f7c45] hover:text-black cursor-pointer transition
                                 hover:shadow-[0_0_12px_#8f7c45]"
-                            >
+                        >
                             <FaInstagram size={18} />
                         </div>
 
                         <div
                             className="w-10 h-10 flex items-center justify-center rounded-full 
-                                border border-[#8f7c45] text-[#8f7c45]
+                                border border-[#fac21e]  text-[#fac21e] 
                                 hover:bg-[#8f7c45] hover:text-black cursor-pointer transition
                                 hover:shadow-[0_0_12px_#8f7c45]"
-                                >
+                        >
                             <FaLinkedinIn size={18} />
                         </div>
                     </div>
