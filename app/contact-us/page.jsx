c0de810ea1119444ed4bbca2a793b8c2a60840e8
ankitNegiDev <ankitnegi9104@gmail.com>
2025-11-27 17:43:10 +0530

    added the contact-us route

---
+++ app/contact-us/page.jsx
@@ -0,0 +1,9 @@
+import ContactUs from "../components/contact-us/ContactUs";
+
+export default function Contact() {
+    return (
+        <main className="pt-20">
+            <ContactUs />
+        </main>
+    );
+}
